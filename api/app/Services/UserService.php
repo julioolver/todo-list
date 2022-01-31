ca2b5227f23fcffd48e00c14c8c105dfffcb4d03
Julio Cesar <juliocesar.olver@gmail.com>
2022-01-30 23:44:43 -0300

    Ajuste do service de user.

--- api/app/Services/UserService.php
+++ api/app/Services/UserService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Exceptions\UserHasBeenTakenException;
+use App\Models\User;
 use App\Repositories\UserRepository;
 
 class UserService
@@ -15,6 +17,25 @@ class UserService
 
     public function update(int $id, array $data)
     {
+        $user = $this->findById($id);
+
+        if (!empty($data['email'] && $this->emailExists($data['email'], $user))) {
+            throw new UserHasBeenTakenException();
+        }
+        if (!empty($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+
         return $this->repository->update($id, $data);
+    }
+
+    public function emailExists(string $email, User $user): bool
+    {
+        return User::where('email', $email)->where('email', '!=', $user->email)->exists();
+    }
+
+    public function findById(int $id): User
+    {
+        return User::find($id);
     }
 }
